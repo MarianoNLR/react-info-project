c266a15b8e878b17354db3269e5b272d042057ed
Mariano <skynesplay@gmail.com>
2022-07-28 06:30:16 -0300

    pagina de error404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-
+import image from '../static/img/img-error404.png';
+import { Link } from 'react-router-dom';
 class Error404 extends Component {
   render() {
     return (
-      <div>Error404</div>
+      <div className='div-error' style={{}}>
+        <span>
+        <img src={image} className='img-error' alt=''/>
+        </span>
+        <p>La pagina solicitada no existe o ha ocurrido un error.<br/>Intente con <Link to='/'>Volver al inicio</Link></p>
+      </div>
     )
   }
 }
